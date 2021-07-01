a1c68b98ccaee74e7a3585c5a9e051e90e2e762d
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-01 14:36:04 +0530

    Update and rename emd_hab.pyx to hab.pyx

--- lynxfall/mdextend/hab.pyx
+++ lynxfall/mdextend/hab.pyx
@@ -1,24 +1,5 @@
 # Extra parsing for markdown (Highlighting and alert boxes)
 
-def parse(rtxt, look, control):
-    txtl = rtxt.split(look) # Turn the text into a string split based on the markdown character we are looking for
-    i, j = 0, 0 # i goes through the list, j is the current state
-    ret_text = "" # The result text
-    while i < len(txtl):
-        if j == 0:
-            # This is the start before the specified tag, add it normally
-            ret_text += control.start(txtl[i]) # Pre tag
-            j+=1
-        elif j == 1:
-            # This is the text we actually want to parse
-            ret_text += control.inner(txtl[i]) # Inner text
-            j+=1
-        else:
-            ret_text += control.end(txtl[i]) # After tag
-            j = 1
-        i+=1
-    return ret_text
-
 # Base Control class
 class Control():
     def start(self, s):
