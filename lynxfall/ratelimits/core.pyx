48bac2bbbac110f4707c1573f03766c26e955a39
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-01 16:33:19 +0530

    Update core.pyx

--- lynxfall/ratelimits/core.pyx
+++ lynxfall/ratelimits/core.pyx
@@ -34,7 +34,7 @@ async def _handle_rl(
     redis
 ):
     if not redis:
-        raise Exception("You must call FastAPILimiter.init in the startup event of FastAPI!")
+        raise Exception("You must call LynxfallLimiter.init in the startup event of FastAPI!")
         
     cdef int index = 0
     
@@ -50,10 +50,10 @@ async def _handle_rl(
     if not rate_key:
         return
         
-    # Key format for per route sublimits is fastapi_limiter.sub-METHOD@URL:KEY#index - These are specific to a single route and keep rotating
-    # Key format for global ratelimits is fastapi_limiter.global-METHOD@URL:KEY - These are global to a API endpoint and blocks you from the whole API for 3 minutes + milliseconds if you hit it
-    # Key format for API wide (ceiling) ratelimit if needed is fastapi_limiter.apiwide:KEY
-    # Key format for API block limit (global and API wide trigger this) is fastapi_limiter.block:KEY. It may or may not have a expiry
+    # Key format for per route sublimits is lynxfall_limiter.sub-METHOD@URL:KEY#index - These are specific to a single route and keep rotating
+    # Key format for global ratelimits is lynxfall_limiter.global-METHOD@URL:KEY - These are global to a API endpoint and blocks you from the whole API for 3 minutes + milliseconds if you hit it
+    # Key format for API wide (ceiling) ratelimit if needed is lynxfall_limiter.apiwide:KEY
+    # Key format for API block limit (global and API wide trigger this) is lynxfall_limiter.block:KEY. It may or may not have a expiry
     method = request.method if request.method not in ("HEAD", "OPTIONS", "CONNECT", "TRACE") else "GET"
     sub_key = f"{prefix}.sub-{method}@{request.url.path}:{rate_key}#{index}" if limits else ""
     global_key = f"{prefix}.global-{method}@{request.url.path}:{rate_key}"
