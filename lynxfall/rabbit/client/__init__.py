bf947692ab54fdc1e0a64ff94031ab361015ec36
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-04 02:26:10 +0530

    Update __init__.py

--- lynxfall/rabbit/client/__init__.py
+++ lynxfall/rabbit/client/__init__.py
@@ -1,7 +1,9 @@
 class Client:
     worker_key: str = None
     redis = None
+    rabbit = None
 
-def setup(worker_key: str, redis):
+def setup(worker_key: str, redis, rabbit):
     Client.worker_key = worker_key
     Client.redis = redis
+    Client.rabbit = rabbit
