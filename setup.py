067c7592a269c3e6b52239180f4fb9820dc9dfeb
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-04 01:08:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,6 @@ setup(
         "lynxfall/mdextend/*.pyx",
         "lynxfall/utils/*.pyx",
         "lynxfall/ratelimits/*.pyx",
-        "lynxfall/rabbit/*.pyx",
-        "lynxfall/rabbit/core/*.pyx",
-        "lynxfall/rabbit/core/default_backends/*.pyx"
     ]),
     packages=["lynxfall", "lynxfall.mdextend", "lynxfall.utils", "lynxfall.ratelimits", "lynxfall.rabbit"],
     zip_safe=False,
