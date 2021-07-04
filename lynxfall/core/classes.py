c136d96fd82457a3d76fd43bb6c2e50dad187ff6
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-04 10:20:00 +0530

    Update classes.py

--- lynxfall/core/classes.py
+++ lynxfall/core/classes.py
@@ -1,7 +1,8 @@
 # Singleton
-class Singleton(type):
+class Singleton(object):
     _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+    def __new__(class_, *args, **kwargs):
+        if class_ not in class_._instances:
+            class_._instances[class_] = super(Singleton, class_).__new__(class_, *args, **kwargs)
+        return class_._instances[class_]
+
