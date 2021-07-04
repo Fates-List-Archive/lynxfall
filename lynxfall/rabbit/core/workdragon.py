63ef1f8b392f788065f6a469de8563f14fde9861
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-04 22:16:16 +0800

    Update workdragon.py

--- lynxfall/rabbit/core/workdragon.py
+++ lynxfall/rabbit/core/workdragon.py
@@ -1,7 +1,27 @@
 """
-
 Work in progress worker management for lynxfall
 
-Note: This will only handle startup/shutdown and other tasks not needed by workers
+Note: This will only handle startup/shutdown and other tasks, not micromanaging individual workers. 
+Stats and error handling is a maybe that will be implemented after core functionality
+Worker-specific tasks are another maybe unless that task is done via redis PUBSUB or other as rabbit is round-robin so not all workers get every task
 
+Workdragon will have a redis pubsub protocol for external management
+
+Support for windows/mac is not planned as of right now.
 """
+
+class Worker():
+    def __init__(self, worker_num, process):
+        self.process = process
+        self.worker_num = worker_num
+    
+class WorkDragon():
+    def __init__(self, launcher):
+        self.workers = []
+        self.launcher = launcher
+        
+    def new_worker(self):
+        proc = subprocess.Popen(['python3', '-u', launcher],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT
+        )
