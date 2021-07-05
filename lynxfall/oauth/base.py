7dc8182db5bcc6a5cf3cc9881baaa2b8a9ed41c8
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-06 00:01:20 +0530

    Update base.py

--- lynxfall/oauth/base.py
+++ lynxfall/oauth/base.py
@@ -1,5 +1,8 @@
 from typing import List
 
-class BaseOauth():  
+class BaseOauth():
+    def __init__(self, auth_jwt_key: str, oc: OauthConfig):
+        self.auth_s = URLSafeSerializer(auth_jwt_key, "auth")
+        
     def get_scopes(self, scopes_lst: List[str]) -> str:
         return "%20".join(scopes_lst)
