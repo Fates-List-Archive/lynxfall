299bda67b252b6ee37d331117e591c5f488895ad
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-06 01:03:21 +0530

    Update models.py

--- lynxfall/oauth/models.py
+++ lynxfall/oauth/models.py
@@ -3,24 +3,27 @@ from typing import Union
 import time
 
 
+class ResponseModel(BaseModel):
+    identifier: str
+    redirect_uri: str
+
 # Core models
 class OauthConfig(BaseModel):
     client_id: str
     client_secret: str
     redirect_uri: str
 
-class OauthURL(BaseModel):
-    identifier: str
+class OauthURL(ResponseModel):
     url: str
     state_id: str
-    redirect_uri: str
 
-class AccessToken(BaseModel):
+class AccessToken(ResponseModel):
     """
     Represents a access token
     
     token_type should always be Bearer and is hence not included
     """
+    
     access_token: str
     refresh_token: str
     expires_in: Union[int, float]
