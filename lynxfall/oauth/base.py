242f17033ebd5bbcf36d723f71496d9627f0464f
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-06 00:16:50 +0530

    Update base.py

--- lynxfall/oauth/base.py
+++ lynxfall/oauth/base.py
@@ -1,9 +1,14 @@
 from typing import List
 from itsdangerous import URLSafeSerializer
+from aioredis import Connection
 
 class BaseOauth():
-    def __init__(self, auth_jwt_key: str, oc: OauthConfig):
+    def __init__(self, auth_jwt_key: str, oc: OauthConfig, redis: Connection):
         self.auth_s = URLSafeSerializer(auth_jwt_key, "auth")
+        self.client_id = oc.client_id
+        self.client_secret = oc.client_secret
+        self.redirect_uri = oc.redirect_uri
+        self.redis = redis
         
     def get_scopes(self, scopes_lst: List[str]) -> str:
         return "%20".join(scopes_lst)
