ce761dd4f6d4cf7fdd7db8f1b16e4f2f9bdfa2da
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-06 16:11:20 +0530

    Update base.py

--- lynxfall/oauth/base.py
+++ lynxfall/oauth/base.py
@@ -156,7 +156,7 @@ class BaseOauth():
         Creates a access token from the state (as JWT) that was created using get_auth_link. 
         Login retry URL is where users can go to login again if login fails.
         
-        For security, use clear_state as True unless you have a reason not to like Fates List does. 
+        For security, use clear_state as True unless you have a reason not to. 
         If you do need clear_state as False, be sure to call clear_state(state_id) to clear the state from the DB.
         """
         state_id = self.get_state_id(state_jwt)
