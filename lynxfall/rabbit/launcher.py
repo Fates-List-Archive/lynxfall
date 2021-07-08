a201c7c67150c261dda594d3c1d2a4f54c2ccf6d
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-08 08:59:21 +0530

    Update launcher.py

--- lynxfall/rabbit/launcher.py
+++ lynxfall/rabbit/launcher.py
@@ -5,24 +5,14 @@ import importlib
 from loguru import logger
 from lynxfall.rabbit.core.process import run_worker, disconnect_worker
 
-async def _runner():
-    while True:
-        try:
-            await asyncio.sleep(0)
-        except KeyboardInterrupt:
-            return
-        except:
-            logger.exception("Something happened and the worker was forced to shut down!")
-
 def run(**run_worker_args):
     try:
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        loop.run_until_complete(run_worker(**run_worker_args))
+        loop.run_forever(run_worker(**run_worker_args))
 
-        # we enter a never-ending loop that waits for data and runs
-        # callbacks whenever necessary.
-        loop.run_until_complete(_runner())         
+        # we have now entered a never-ending loop that waits for data and runs
+        # callbacks whenever necessary. 
     except KeyboardInterrupt:
         try:
             loop = asyncio.new_event_loop()
