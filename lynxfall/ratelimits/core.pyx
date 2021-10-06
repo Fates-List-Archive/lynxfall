001b9acee5fb7288f66bd2c798d29a35b18d8e9c
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-10-06 09:12:56 +0530

    add operation buckets

--- lynxfall/ratelimits/core.pyx
+++ lynxfall/ratelimits/core.pyx
@@ -31,7 +31,8 @@ async def _handle_rl(
     prefix,
     identifier, 
     callback,
-    redis
+    redis,
+    operation_bucket = None
 ):
     if not redis:
         raise Exception("You must call LynxfallLimiter.init in the startup event of FastAPI!")
@@ -54,7 +55,12 @@ async def _handle_rl(
     # Key format for global ratelimits is lynxfall_limiter.global-METHOD@URL:KEY - These are global to a API endpoint and blocks you from the whole API for 3 minutes + milliseconds if you hit it
     # Key format for API wide (ceiling) ratelimit if needed is lynxfall_limiter.apiwide:KEY
     # Key format for API block limit (global and API wide trigger this) is lynxfall_limiter.block:KEY. It may or may not have a expiry
-    method = request.method if request.method not in ("HEAD", "OPTIONS", "CONNECT", "TRACE") else "GET"
+    if not operation_bucket:
+        method = request.method if request.method not in ("HEAD", "OPTIONS", "CONNECT", "TRACE") else "GET"
+        path = request.url.path
+    else:
+        method = operation_bucket
+        path = "op"
     sub_key = f"{prefix}.sub-{method}@{request.url.path}:{rate_key}#{index}" if limits else ""
     global_key = f"{prefix}.global-{method}@{request.url.path}:{rate_key}"
     api_block_key = f"{prefix}.block:{rate_key}"
@@ -93,6 +99,7 @@ async def _handle_rl(
         response.headers[f"Requests-{ext}-Remaining"] = str(strategy.times - num) if num <= strategy.times else "-1" # Requests remaining in time window
         response.headers[f"Requests-{ext}-Window"] = str(strategy.milliseconds/1000)
         response.headers[f"Requests-{ext}-Expiry-Time"] = str(pexpire)
+        response.headers["Request-Bucket"] = method
         return response
         
     response = _set_headers(response, strategy, pexpires, nums, ext = "Sub") if limits else response
