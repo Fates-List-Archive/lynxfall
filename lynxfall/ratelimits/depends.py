7fc652c643912797021bf5d4118c5acbe46c1edc
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-12-21 23:05:32 +0530

    Update depends.py

--- lynxfall/ratelimits/depends.py
+++ lynxfall/ratelimits/depends.py
@@ -17,8 +17,13 @@ class Ratelimiter:
     ):
         if not global_limit:
             global_limit = []
+        
         elif not isinstance(global_limit, list):
             global_limit = [global_limit]
+            
+        if not sub_limits:
+            sub_limits = []
+            
         self.sub_limits = sub_limits
         self.global_limit = global_limit
         self.operation_bucket = operation_bucket
@@ -27,7 +32,7 @@ class Ratelimiter:
         return await _handle_rl(
             request = request, 
             response = response,
-            limits = self.sub_limits,
+            limits = self.global_limit + self.sub_limits,
             global_limit = self.global_limit,
             identifier = LynxfallLimiter.identifier or self.identifier,
             callback = LynxfallLimiter.callback or self.callback,
