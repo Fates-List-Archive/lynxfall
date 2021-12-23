d1309324c065d85da6d0250870568724b31a3cb5
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-12-23 17:01:07 +0530

    Update string.pyx

--- lynxfall/utils/string.pyx
+++ lynxfall/utils/string.pyx
@@ -61,5 +61,5 @@ def intl_text(text: str, lang: str, dbg: bool = False):
         if lang_tag.name == "fl-lang" and lang_tag.get("code", "") not in (lang, "default", ""):
             lang_tag.decompose()
         
-    return str(soup.body or soup)
+    return str(soup.body.p)
 
